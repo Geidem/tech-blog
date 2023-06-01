f9ed871bff44dd0b7a00450e1f9e01741f9ddcd7
Unbroken <gibby.eidem@gmail.com>
2023-05-31 23:58:43 -0600

    Added utils folder withAuth to work with api routes

--- public/login.js
+++ public/login.js
@@ -2,6 +2,25 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-login')
+    const password = document.querySelector('#password-login')
     fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(response => {
+        document.location.replace('/dashboard');
+    })
+    .catch(err => {
+        console.log(err);
+    }
+    );
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
